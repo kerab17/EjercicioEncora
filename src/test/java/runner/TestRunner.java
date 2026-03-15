27310a6d4cca638a8ff38e7c56b987d107529411
brayankevin43 <brayankevin43@gmail.com>
2026-03-15 01:04:37 -0500

    Se agrego como comentario el autor

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,10 +6,10 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 
-
+//Estos cambios los hizo kevin ricalde
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/features",
+        features = "src/test/java/features/login.feature",
         glue = {"utility", "stepDefinition"},
         plugin = {"pretty", "html:target/cucumber-html-report","json:cucumber.json"}
 
